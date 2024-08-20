facb6c13896a64fd64dbf278414edba0e49a6704
ggnaml <148931744+ggnaml@users.noreply.github.com>
2024-08-20 10:44:15 +0700

    percobaan1, percobaan2, percobaan3

---
+++ Coba1gm/src/coba1gm/Coba1gm.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package coba1gm;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Coba1gm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Coba1gm/src/coba1gm/JOption.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coba1gm;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class JOption {
+
+    static class Pane {
+
+        public Pane() {
+        }
+    }
+    
+}
