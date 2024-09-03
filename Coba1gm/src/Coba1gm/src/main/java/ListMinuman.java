d2768371b49b147995c3349a5a5b95b6917cb2fc
ggnaml <148931744+ggnaml@users.noreply.github.com>
2024-09-02 22:09:40 -0700

    Add files via upload
    Percocobaan6, Percobaan7, Percobaan8 tapi beleum selesai

---
+++ Coba1gm/src/Coba1gm/src/main/java/ListMinuman.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class ListMinuman {
+
+    static void setModel(JList<String> ListModel) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
